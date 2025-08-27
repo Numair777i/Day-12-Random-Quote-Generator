d680227fcd86c19be235ad909cddb3a90f21fd73
numair777i <numairnizami1+code@gmail.com>
2025-08-27 12:35:08 +0530

    update commit

--- script.js
+++ script.js
@@ -1,24 +1,15 @@
-const quotes=[
-  "Be yourself; everyone else is already taken. – Oscar Wilde",
-  "In the middle of every difficulty lies opportunity. – Albert Einstein",
-  "Do what you can, with what you have, where you are. – Theodore Roosevelt",
-  "Happiness depends upon ourselves. – Aristotle",
-  "It always seems impossible until it’s done. – Nelson Mandela",
-  "Don’t count the days, make the days count. – Muhammad Ali",
-  "Your time is limited, so don’t waste it living someone else’s life. – Steve Jobs",
-]
+const quote = document.querySelector("#quote");
+const author = document.querySelector("#author");
+const api_url = "https://thequoteshub.com/api/";
 
-const quoteElement=document.querySelector("#quote");
-
-const usedIndexes=new Set();
-
-function generateQuote(){
-  while(true){
-  const randomIdx=Math.floor(Math.random()*quotes.length);
-  if(usedIndexes.has(randomIdx))continue
-  const quote=quotes[randomIdx];
-  quoteElement.innerHTML=quote;
-  usedIndexes.add(randomIdx);
-  break
-  }
+async function getquote(){
+  const response = await fetch(api_url);
+  var data = await response.json();
+  console.log(data);
+  // quote.innerHTML = data.quote;
+  // author.innerHTML = data.author;
+   quote.textContent = data.text;
+  author.textContent = data.author;
 }
+
+getquote();
